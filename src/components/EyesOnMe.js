b85e909f5b09e651e9a2600095058832f411d02c
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-31 18:12:35 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+
+const focus = (e) => {
+  console.log('Good!');
+}
+
+const blur = (e) => {
+  console.log('Hey! Eyes on me!');
+}
+
+  return (
+    <div>
+      <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function event() {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input type="password" onChange={event} />
+    </div>
+  );
 }
 
 export default Keypad;
